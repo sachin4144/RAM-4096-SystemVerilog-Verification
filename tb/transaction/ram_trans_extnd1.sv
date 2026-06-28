a3d837fec77c0f968b9c76bac5daa8ff7f153d01
Sachin Kumar Mishra <149581959+sachin4144@users.noreply.github.com>
2026-06-28 16:47:44 +0530

    Create ram_trans_extnd1.sv

---
+++ tb/transaction/ram_trans_extnd1.sv
@@ -0,0 +1,6 @@
+//class ram_trans_extnd1
+class ram_trans_extnd1 extends ram_trans;
+      
+   //define a constraint valid_random_data with data inside the range which is not covered 
+   constraint valid_random_data {data inside {[1501:2000],4294};}
+   //defin
